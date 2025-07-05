80202e103f076c9246c1137c63c3670335c0b773
GuiCarminati <guicarminati@outlook.com>
2025-07-05 16:29:00 +0100

    Create CRUD minions api

--- medium_projects/boss-machine/server/api.js
+++ medium_projects/boss-machine/server/api.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const errorhandler = require('errorhandler');
+const apiMinions = require('./apiMinions');
+
 const apiRouter = express.Router();
 
+// .use with error handler was here before
 
+apiRouter.use('/minions',apiMinions);
+
+apiRouter.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
+// apiRouter.use(errorhandler(false));
 
 module.exports = apiRouter;
--- medium_projects/boss-machine/server/apiMinions.js
+++ medium_projects/boss-machine/server/apiMinions.js
@@ -1,13 +1,70 @@
 const express = require('express');
 const apiMinions = express.Router();
-const db = require('db.js');
+const db = require('./db.js');
+const { createError } = require('./utils.js');
+
+const type = 'minions';
 
 apiMinions.get('/', (req,res,next) => {
-    res.status(200).send()
+    const list = db.getAllFromDatabase(type);
+    res.status(200).send(list);
+});
+
+apiMinions.param('id',(req,res,next,id)=>{
+    const found = db.getFromDatabaseById(type,id);
+    if(found){
+        req.found = found;
+        next();
+    } else {
+        next(`Resource ${type} with id ${id} not found`,404);
+    }
 });
 
 
+apiMinions.get('/:id',(req,res,next)=>{
+    res.status(200).send(req.found);
+});
 
+
+apiMinions.put('/:id',(req,res,next)=>{
+    const original = req.found;    
+    const newValues = req.body;
+    const newInstance = {
+        id: original.id,
+        name: newValues.name || original.name,
+        title: newValues.title || original.title,
+        weaknesses: newValues.weaknesses || original.weaknesses,
+        salary: newValues.salary || original.salary
+    }
+    const updated = db.updateInstanceInDatabase(type,newInstance);
+    if(updated){
+        res.status(200).send(updated);
+    } else {
+        next(createError('Failed to update'));
+    }
+});
+
+apiMinions.delete('/:id',(req,res,next) => {
+    const id = req.found.id;
+    db.deleteFromDatabasebyId(type,id);
+    res.status(204).send(`Resource ${type} with id ${id} deleted`);
+});
+
+apiMinions.post('/',(req,res,next)=>{
+    const newValues = req.body;
+    const newInstance = {
+        name: newValues.name,
+        title: newValues.title,
+        weaknesses: newValues.weaknesses,
+        salary: newValues.salary
+    }
+    const created = db.addToDatabase(type,newInstance);
+    if(created){
+        res.status(200).send(created);
+    } else {
+        next(createError('Failed to create'));
+    }
+});
 
 
 module.exports = apiMinions;
